ada6d5395b80c65d63d56cca35f0467a0a4715d6
Laura P <ljp013@bucknell.edu>
2017-06-21 10:19:54 -0500

    biolinks tutorial initial commit

---
+++ TCGAbiolinks.R
@@ -0,0 +1,11 @@
+#Laura Poulton
+#MSCS REU
+#6/21/17
+
+#TCGA biolinks tutorial
+#from: https://bioconductor.org/packages/release/bioc/vignettes/TCGAbiolinks/inst/doc/index.html
+
+
+#to install
+source("https://bioconductor.org/biocLite.R")
+biocLite("TCGAbiolinks")
